a471b6519f5f1901431b9b98e4ba02179c6ed554
Tom <zefyras000@gmail.com>
2018-12-19 22:37:15 +0200

    Semi internal counter c#

---
+++ Somewhat of internal counter/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var counter = Properties.Settings.Default.Counter;
+            if (counter == 0)
+            {
+                Console.WriteLine("Program will run 5 times");
+                IncreaseCounter();
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            if (counter <= 5)
+            {
+                IncreaseCounter();
+                Console.WriteLine($"Hello there,\nApp ran {counter} times, you have {5 - counter} times left");
+            }
+            else
+            {
+                Console.WriteLine("license is over\nType key");
+                var key = Console.ReadLine();
+                if (key == "123456")
+                    Properties.Settings.Default.Reset();
+                Environment.Exit(0);
+            }
+
+            Console.ReadKey();
+            
+
+        }
+
+        static void IncreaseCounter()
+        {
+            Properties.Settings.Default.Counter++;
+            Properties.Settings.Default.Save();
+        }
+
+
+
+
+    }
+}
---
+++ Somewhat of internal counter/README.md.txt
@@ -0,0 +1,11 @@
+Path to exe settings file is \AppData\Local\test
+this is stored as user settings.
+
+Application settings is immutable you can't write to it at runtime.
+
+When you move exe it starts counter from new.
+Renaming app also causes this too.(or removing app data folder)
+
+Pass is 123456.
+
+

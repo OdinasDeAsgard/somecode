1d147496f0532880e916868d3b9f218f23c0c4eb
Tom <zefyras000@gmail.com>
2018-12-21 20:52:53 +0200

    din

---
+++ Dynamic compilation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var csc = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", "v3.5" } });
+        var parameters = new CompilerParameters(new[] { "mscorlib.dll", "System.Core.dll" }, "foo.exe", true);
+        parameters.GenerateExecutable = true;
+
+        Console.WriteLine("iveskite pasisveikinima");
+        var hey = Console.ReadLine();
+
+        CompilerResults results = csc.CompileAssemblyFromSource(parameters,
+        @"using System.Linq;
+          using System;
+            class Program {
+              public static void Main(string[] args)
+              {
+               Console.WriteLine(""" + hey + @""");
+                Console.ReadKey();
+              }
+            }");
+        results.Errors.Cast<CompilerError>().ToList().ForEach(error => Console.WriteLine(error.ErrorText));
+    }
+}
